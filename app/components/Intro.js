d1e6ecbbbd586534fda2af13b1e380c272102ea7
Sachiko Kwan <sachiko@nextsmallthings.com>
2016-10-20 15:30:29 -0700

    Temporary intro page to set the trainer level

---
+++ app/components/Intro.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  TouchableHighlight,
+  TextInput,
+  View
+} from 'react-native';
+
+import { Input, Text, Button } from 'native-base';
+
+class Intro extends Component {
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          What was (or is) your level at the time you took the screenshots?
+        </Text>
+        
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder='Enter Trainer Level'/>
+          <Button>Scan Photos</Button>
+        </View>
+      </View>
+
+    );
+  }
+}
+const styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 200,
+    alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  
+  searchInput: {
+    height: 40,
+    padding: 4,
+    marginRight: 5,
+    flex: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    borderRadius: 5,
+    color: '#48BBEC'
+  },
+
+});
+
+module.exports = Intro;
